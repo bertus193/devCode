798e3e687699f2a8ed7dde699f5fb346dae80eb5
Alberto Rico Garcia <albrigar93@gmail.com>
2018-01-26 11:35:20 +0100

    Cambiado mensaje Success

--- public/js/devCode.js
+++ public/js/devCode.js
@@ -10,6 +10,6 @@ function publicSuccessMsg(successMsg) {
     document.getElementById("notificationMsg").innerHTML =
         '<div class="alert alert-dismissible alert-success">\
                     <button type="button" class="close" data-dismiss="alert">&times;</button>\
-                    <strong>Error:</strong><br>' + successMsg + '.\
+                    <strong>Ya está:</strong><br>' + successMsg + '.\
                     </div>'
 }
